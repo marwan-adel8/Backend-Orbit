09e777773f113266facf4cb4039d61213fc500cb
marwan-adel8 <adelmarawan638@gmail.com>
2026-05-24 13:23:49 +0300

    lastt

--- config/db.js
+++ config/db.js
@@ -1,20 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI environment variable is missing!");
-    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB Connected:`);
-    global.dbError = null; // Reset error on success
+    console.log(`✅ MongoDB Connected`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
-    global.dbError = error.message; // Store the error message
-    // Do not call process.exit(1) on Vercel as it crashes the Serverless Function container immediately.
-    if (!process.env.VERCEL) {
-      process.exit(1);
-    }
+    process.exit(1);
   }
 };
 
